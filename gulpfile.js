dca6d2d88da082652deeb0c59af413803fe617a4
ayame30 <alnaym30@gmail.com>
2017-01-14 03:43:45 +0800

    Use hbs

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
+var hbs = require('gulp-hbs');
 
 gulp.task('watch', function () {
 	gulp.watch('./scss/**/*.scss', ['sass']);
+	gulp.watch('./data/*.json', ['hbs']);
 });
 
 gulp.task('sass', function(){
 	gulp.src('./scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./styles/'));
+		.pipe(gulp.dest('./html/styles/'));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('hbs', function () {
+	gulp.src('./data/**/*.json')
+ 	   .pipe(hbs(gulp.src('templates/index.hbs')))
+ 	   .pipe(gulp.dest('./html'))
+	   .on('error', sass.logError);
+});
+
+gulp.task('default', ['hbs','watch']);
